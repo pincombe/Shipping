3a39d2117a9171541899b9a6914dafc469d3c620
Keith Pincombe <pincombe@gmail.com>
2013-01-14 13:49:02 +0000

    added missing product class

---
+++ src/Shipping/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Shipping;
+
+class ProductException extends \Exception {}
+
+class Product
+{
+	public $id;
+    public $name;
+    public $stock_id;
+    public $last_ordered;
+
+    private $account_id;
+
+    public function __construct($id, $name, $stock_id, $account_id, $last_ordered)
+	{
+		$this->id = $id;
+		$this->name = $name;
+		$this->stock_id = $stock_id;
+		$this->account_id = $account_id;
+		$this->last_ordered = $last_ordered;
+	}
+
+	public function getAccountID()
+	{
+		return $this->account_id;
+	}
+
+
+}
