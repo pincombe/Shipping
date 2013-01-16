6d62395a7520796650441d812ce19c0ef5a15d50
Keith Pincombe <pincombe@gmail.com>
2013-01-16 16:39:46 +0000

    removed account_id from config class

--- src/Shipping/Config.php
+++ src/Shipping/Config.php
@@ -6,13 +6,11 @@ class ConfigException extends \Exception {}
 
 class Config
 {
-	public $id;
 	public $kunaki_user;
 	public $kunaki_pass;
 
-    public function __construct($id, $kunaki_user, $kunaki_pass)
+    public function __construct($kunaki_user, $kunaki_pass)
 	{
-		$this->id = $id;
 		$this->kunaki_user = $kunaki_user;
 		$this->kunaki_pass = $kunaki_pass;
 	}
