cf6980f5a78f43065c2600f43fb19d77100994f9
Keith Pincombe <pincombe@gmail.com>
2013-01-17 11:20:52 +0000

    added some validation and put function

--- src/Shipping/Product.php
+++ src/Shipping/Product.php
@@ -18,4 +18,31 @@ class Product
 		$this->stock_id = $stock_id;
 		$this->last_ordered = $last_ordered;
 	}
+
+	public static function load($json)
+	{
+		$data = json_decode($json);
+
+		if (empty($data)) {
+			throw new ProductException('Unable to decode json object');
+		}
+
+		return new self($data->id, $data->name, $data->stock_id, $data->last_ordered);
+	}
+
+	public static function loadAll($json)
+	{
+		// TODO: Empty array can be valid but an empty response or undecodable data isn't
+
+		$data = json_decode($json);
+
+		$products = array();
+		foreach ($data as $product) {
+			$products[] = new self($product->id, $product->name, $product->stock_id, $product->last_ordered);
+		}
+
+		return $products;
+	}
+
+
 }
