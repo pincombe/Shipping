f8e1b582cff97f887e427d91387c2075ac7d4c20
Keith Pincombe <pincombe@gmail.com>
2013-01-17 12:24:41 +0000

    made parent calls protected

--- src/Shipping/Product/Client.php
+++ src/Shipping/Product/Client.php
@@ -11,31 +11,30 @@ class Client extends \Transmit\Client
 
 	public function fetchAll()
 	{
-		$response = $this->get('/product');
+		$response = $this->_get('/product');
 		return \Shipping\Product::loadAll($response);
 	}
 
 	public function fetch($id)
 	{
-		$response = $this->get(sprintf('/product/%d', $id));
+		$response = $this->_get(sprintf('/product/%d', $id));
 		return \Shipping\Product::load($response);
 	}
 
 	public function save(Product $product)
 	{
 		if ($product->id == 0) {
-			$response = $this->post('/product', json_encode($product));
+			$response = $this->_post('/product', json_encode($product));
 			return \Shipping\Product::load($response);
 		}
 
-		$response = $this->put(sprintf('/product/%d', $product->id), json_encode($product));
+		$response = $this->_put(sprintf('/product/%d', $product->id), json_encode($product));
 		return \Shipping\Product::load($response);
 	}
 
 	public function delete($id)
 	{
-		$response = $this->delete(sprintf('/product/%d', $id));
-		return json_decode($response);
+		$this->_delete(sprintf('/product/%d', $id));
 	}
 
 }
